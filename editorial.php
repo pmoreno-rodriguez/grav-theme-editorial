8e4a30fc9f1d6a1c4b8672c00f396f7f76cc2e17
pmoreno-rodriguez <pmorenorodriguez@protonmail.com>
2025-09-30 21:51:19 +0200

    New `WordCount` twig filter

--- editorial.php
+++ editorial.php
@@ -4,6 +4,7 @@ namespace Grav\Theme;
 use Grav\Common\Theme;
 use RocketTheme\Toolbox\Event\Event;
 use Grav\Common\Page\Interfaces\PageInterface;
+use Grav\Theme\Editorial\WordCountTwigExtension;
 
 /**
  * Editorial Theme
@@ -26,7 +27,8 @@ class Editorial extends Theme
     {
         return [
             'onShortcodeHandlers' => ['onShortcodeHandlers', 0],
-            'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+            'onTwigExtensions' => ['onTwigExtensions', 0]
         ];
     }
 
@@ -35,23 +37,33 @@ class Editorial extends Theme
         $this->grav['shortcode']->registerAllShortcodes(__DIR__ . '/shortcodes');
     }
 
+    /**
+     * Register custom CSS or JavaScript assets
+     **/
     public function onTwigSiteVariables()
     {
-        if ($this->isAdmin() && ($this->grav['config']->get('plugins.shortcode-core.enabled'))) {
-        }
-
-        $themeConfig = $this->config->get('themes.editorial');
-
+        // Get active theme dynamically
+        $activeTheme = $this->grav['theme']->name;
+        $themeConfig = $this->config->get("themes.$activeTheme");
+        
+        // Register custom CSS
         $custom_css_path = $this->grav['locator']->findResource('theme://assets/css/custom.css');
-
         if (isset($themeConfig['custom_css']) && $themeConfig['custom_css'] && $custom_css_path) {
-            $this->grav['assets']->addCss('theme://assets/css/custom.css', ['priority' => 5]);
+            $this->grav['assets']->addCss('theme://assets/css/custom.css', ['priority' => 10]);
         }
-
-       $custom_js_path = $this->grav['locator']->findResource('theme://assets/js/custom.js');
-
+        
+        // Register custom JavaScript
+        $custom_js_path = $this->grav['locator']->findResource('theme://assets/js/custom.js');
         if (isset($themeConfig['custom_js']) && $themeConfig['custom_js'] && $custom_js_path) {
             $this->grav['assets']->addJs('theme://assets/js/custom.js', ['group' => 'bottom', 'priority' => 15]);
         }
+    }
+
+    public function onTwigExtensions()
+    {
+        $twig = $this->grav['twig'];
+        // Include and add WordCount extension
+        include_once __DIR__ . '/twig/WordCountTwigExtension.php';
+        $twig->twig->addExtension(new WordCountTwigExtension()); 
     }
 }
